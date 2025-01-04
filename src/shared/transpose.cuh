14b0435383ba8fc43186f3e8fe09f4323234cc50
Rémi Jupin <remi.jupin@grenoble-inp.org>
2025-01-05 00:05:34 +0100

    Transposition de matrices fonctionnel et optimisé

---
+++ src/shared/transpose.cuh
@@ -0,0 +1,27 @@
+__global__ void
+transpose(float* A, float* T, int heightA, int widthA) // A: source matrix; T: matrix to be transposed
+{
+  int Ax = blockDim.x * blockIdx.x + threadIdx.x;
+  int Ay = blockDim.y * blockIdx.y + threadIdx.y;
+
+  if (Ax < widthA && Ay < heightA) {
+
+    int indexA = Ax + widthA * Ay;
+
+    int blockDimY = blockDim.y + 1;
+    extern __shared__ float tile[]; // blockDimY = blockDim.y + 1 to avoid bank conflicts
+
+    int indexTile = threadIdx.y + blockDimY * threadIdx.x;
+
+    tile[indexTile] = A[indexA]; // Transposition
+
+    __syncthreads();
+
+    indexTile = threadIdx.x + blockDimY * threadIdx.y;
+    int indexT = blockDim.y * blockIdx.y + threadIdx.x + heightA * (blockDim.x * blockIdx.x + threadIdx.y);
+    T[indexT] = tile[indexTile]; // Write to destination matrix
+  }
+  else {
+    printf("Index Ax or Ay out of bounds: Ax = %i, Ay = %i\n", Ax, Ay);
+  }
+}
