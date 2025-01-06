f3a50382880a2ddce1b63231e66835a46b16d68d
Matéo Sassier <mateo.sassier@grenoble-inp.org>
2025-01-06 09:14:43 +0100

    Librairies lecture image

--- src/shared/im2tensor.hpp
+++ src/shared/im2tensor.hpp
@@ -1,8 +1,7 @@
-#include <string>
 
 /*
 Image Processing: Load the image and put it into a tensor.
     image: string
     tensorImage: float*
 */
-void im2tensor(float* tensorImage, std::string image);
+void im2tensor(float* image_float, char* chemin_image);
