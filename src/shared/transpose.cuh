e7c6dedb84bff381589a08f557021496ed1cbde1
Rémi Jupin <remi.jupin@grenoble-inp.org>
2025-02-05 14:18:35 +0100

    debug transpose.cuh + get_args

--- src/shared/transpose.cuh
+++ src/shared/transpose.cuh
@@ -1,3 +1,9 @@
+void get_transpose_args(dim3 &grid, dim3 &threads, int &shared, int block_size, int height, int width) {
+  threads = dim3(block_size, block_size);
+  grid = dim3(height / threads.x, width / threads.y);
+  shared = block_size * (block_size+1) * sizeof(float);
+}
+
 __global__ void
 transpose(float* A, float* T, int heightA, int widthA) // A: source matrix; T: matrix to be transposed
 {
@@ -11,7 +17,7 @@ transpose(float* A, float* T, int heightA, int widthA) // A: source matrix; T: m
     int blockDimY = blockDim.y + 1;
     extern __shared__ float tile[]; // blockDimY = blockDim.y + 1 to avoid bank conflicts
 
-    int indexTile = threadIdx.y + blockDimY * threadIdx.x;
+    int indexTile = threadIdx.x + blockDimY * threadIdx.y;
 
     tile[indexTile] = A[indexA]; // Transposition
 
