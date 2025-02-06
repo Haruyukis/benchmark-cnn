1a133500ddc7f0b8e26b6fd0de6b61d9ada5532b
Alex Jiang <talexjiang@gmail.com>
2025-02-06 12:54:12 +0100

    WIP: Tile pre-process and post process.

--- src/winograd/rearange_tile.cpp
+++ src/winograd/rearange_tile.cpp
@@ -1,20 +1,95 @@
-#include "rearange_tile.hpp"
+#include <iostream>
+using namespace std;
 
-void input_to_tiles(float *input)
+/**
+ * Reshapes an `n x n` image into `nb_tiles` tiles of `4x4`, stored in row-major order.
+ *
+ * @param image: Input image stored as a `float*` array of size `n * n`.
+ * @param n: The size of the image (assumed to be square: `n x n`).
+ * @param output: Output image stored as a `float*` array of size `n * n`.
+ */
+void reshape_into_patches(const float *image, float *output, int n)
 {
+    int tile_size = 4;
+    int tile_per_row = n / tile_size;
+    int nb_tiles = tile_per_row * tile_per_row;
+
+    int tile_idx = 0;
+    for (int i = 0; i < n; i += tile_size)
+    { // Iterate over row blocks
+        for (int j = 0; j < n; j += tile_size)
+        { // Iterate over column blocks
+            // Fill each 4x4 patch
+            for (int x = 0; x < tile_size; x++)
+            {
+                for (int y = 0; y < tile_size; y++)
+                {
+                    output[tile_idx * tile_size * tile_size + x * tile_size + y] =
+                        image[(i + x) * n + (j + y)];
+                }
+            }
+            tile_idx++;
+        }
+    }
 }
 
-void tiles_to_output(float *output_tiles, float *output, int nb_tiles, int output_width)
+/**
+ * Prints an array of `float` values in a grid format.
+ */
+void print_patches(float *tile, int nb_tiles, int tile_size)
 {
-    for (int tile_idx = 0; tile_idx < nb_tiles; tile_idx++)
+    for (int i = 0; i < nb_tiles; i++)
     {
-        int old_base = tile_idx * 4;
-        int new_base = (tile_idx % (output_width / 2)) * 2 + (tile_idx / (output_width / 2)) * 2 * output_width;
+        cout << "Tile " << i << ":\n";
+        for (int x = 0; x < tile_size; x++)
+        {
+            for (int y = 0; y < tile_size; y++)
+            {
+                cout << tile[i * tile_size * tile_size + x * tile_size + y] << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
 
-        output[new_base] = output_tiles[old_base];
-        output[new_base + 1] = output_tiles[old_base + 1];
+    for (int i = 0; i < 144; i++)
+    {
+        cout << tile[i] << " ";
+    }
+}
+void rearrange_to_2x2_grid_major(float *input_image, float *output_image, int n)
+{
+    // Each block is 2x2; there are (n/2) blocks per row.
+    int block_size = 2;
+    int blocks_per_row = n / block_size;
+    int num_blocks = blocks_per_row * blocks_per_row; // total number of 2x2 blocks
 
-        output[new_base + output_width] = output_tiles[old_base + 2];
-        output[new_base + output_width + 1] = output_tiles[old_base + 3];
+    int current = 0; // index into input_image (which is in linear order)
+    for (int b = 0; b < num_blocks; b++)
+    {
+        // Determine the block's position in the output grid:
+        int r_block = b / blocks_per_row; // block row index (0..blocks_per_row-1)
+        int c_block = b % blocks_per_row; // block column index (0..blocks_per_row-1)
+        // The starting coordinates (row, col) for this block in the output matrix:
+        int base_row = r_block * block_size;
+        int base_col = c_block * block_size;
+
+        // Fill the 2x2 block (in row-major order) with the next 4 consecutive input elements:
+        output_image[(base_row)*n + (base_col)] = input_image[current++];
+        output_image[(base_row)*n + (base_col + 1)] = input_image[current++];
+        output_image[(base_row + 1) * n + (base_col)] = input_image[current++];
+        output_image[(base_row + 1) * n + (base_col + 1)] = input_image[current++];
+    }
+}
+
+void print_matrix(float *matrix, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cout << matrix[i * n + j] << "\t";
+        }
+        cout << endl;
     }
 }
--- src/winograd/rearange_tile.hpp
+++ src/winograd/rearange_tile.hpp
@@ -1,9 +1,9 @@
 /**
  * True input to input tile arrangement
  */
-void input_to_tiles(float *input);
+void reshape_into_patches(const float *image, float *output, int n);
 
 /**
  * Output tile arrangement to true output.
  */
-void tiles_to_output(float *output);
+void rearrange_to_2x2_grid_major(float *input_image, float *output_image, int n);
