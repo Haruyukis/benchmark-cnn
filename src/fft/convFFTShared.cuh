99e2a1dcac931a2f02cc60e923dcb9b6a74fc56b
Matéo Sassier <mateo.sassier@grenoble-inp.org>
2025-02-08 18:29:29 +0100

    Entete pour ConvFFTShared

---
+++ src/fft/convFFTShared.cuh
@@ -0,0 +1,10 @@
+#include <cuComplex.h>
+
+/* Convolution by using FFT transform of imgDevice and kernelDevice
+    img_complexe: image on the host ->> only for tests TO BE REMOVED
+    imgDevice: image on the device
+    kernelDevice: kernel on the device
+    width: width of the image
+    height: height of the image
+    channels: number of channels of the image*/
+void convFFTShared(cuFloatComplex** img_complexe, cuFloatComplex* imgDevice, cuFloatComplex* kernelDevice, int width, int height, int channels);
